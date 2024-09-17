66cfffe205100d9b8db93a2a1c00c64b22a30a0c
dhrruvchotai <23010101051@darshan.ac.in>
2024-09-17 19:55:01 +0530

    added to demohooks for FetchAPI

--- ReactJs/demohooks/src/App.jsx
+++ ReactJs/demohooks/src/App.jsx
@@ -1,26 +1,36 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import Counter from './Components/Counter';
+import FetchAPI from './Components/FetchAPI';
 function App() {
   
   //Case 1 : Run on every render. (because the dependency array is not passed.)
-  useEffect(()=>{
-    alert("Hey i will run on every render.")
-  });
+  // useEffect(()=>{
+  //   alert("Hey i will run on every render.")
+  // });
 
   //Case 2 : Run only on first render.(because empty dependency array is passed.)
-  useEffect(() => {
-    alert('I will only run on the first render.')
-  },[]);
+  // useEffect(() => {
+  //   alert('I will only run on the first render.')
+  // },[]);
 
   //Case 3 : When some state changes (dependency array is passed with some value.)
     //for this see Counter and Color components.
   return (
     <>
-      <div>
+
+      {/* ------------------------------------- */}
+      {/* For useEffect */}
+      {/* <div>
         <h1>Counter Component</h1>
         <Counter /> 
-      </div>
+      </div> */}
+      {/* ----------------------------------------- */}
+
+
+      {/* ------------------------------------ */}
+      {/* For API fetching. */}
+      <FetchAPI/>
     </>
   );
 }
---
+++ ReactJs/demohooks/src/Components/FetchAPI.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+
+const FetchAPI = () => {
+
+    const [data, setData] = useState([]);
+    const [varname, setVarname] = useState("batman");
+
+    async function fetchData(varname) {
+        try {
+            const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=14d435fd&s=${varname}`);
+            const result = await response.json();
+            setData(result.Search);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    SearchChange = (obj) => {
+        setVarname(obj.target.value);  // Update the search term on input change
+    };
+
+    SearchSubmit = (obj) => {
+        obj.preventDefault();
+        fetchData(varname);  // Fetch data based on the updated search term when the form is submitted
+    };
+
+    let finalData = data.map((obj) => {
+        return (
+            <>
+                <div className='col col-4'>
+                    <div className="card" style={{ width: 18 + "rem" }}>
+                        <img src={obj.Poster} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{obj.Title}</h5>
+                            <p className="card-text">{obj.Year}</p>
+                            <a href="#" className="btn btn-primary">Watch Now</a>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    });
+
+    return (
+        <>
+            <form className="d-flex mb-3" role="search" onSubmit={SearchSubmit}>
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"  onChange={SearchChange}/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+            <div className='container'>
+                <div className='row'>{finalData}</div>
+            </div>
+        </>
+    )
+}
+
+
+
+
+
+
+export default FetchAPI
